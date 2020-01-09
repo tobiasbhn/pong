7a2e29f485d77aaf74758a9b521843677124112f
Tobias Bohn <info@tobiasbohn.com>
2020-01-09 16:56:59 +0100

    [TASK] reworked routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   # For all normal Frontend-Pages
   root 'application#index', as: 'index'
   get '/host', to: 'application#host', as: 'new_game'
-  get '/(:game_id)/join', to: 'application#join', as: 'new_user'
+  post '/host', to: 'game#create', as: 'create_game'
 
-  # Backend Stuff
-  post '/game/create', to: 'game#create', as: 'create_game'
-  post '/user/create', to: 'user#create', as: 'create_user'
-  post '/user/auth', to: 'user#auth', as: 'user_auth'
+  get '/join', to: 'application#join', as: 'new_user'
+  post '/join', to: 'user#create', as: 'create_user'
 
 
   # 'Secured'
   get '/:game_id', to: 'application#show', as: 'game', constraints: { game_id: /\d{5}/ }
+  post '/:game_id', to: 'user#auth', as: 'user_auth', constraints: { game_id: /\d{5}/ }
 end
