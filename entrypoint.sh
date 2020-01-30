5e6ad487d4ac5a0034228e70a35efd9e076af7d2
Tobias Bohn <tobiasbohn1998@gmail.com>
2020-01-30 17:58:12 +0100

    [FEATURE] added healthcheck for postgres
    * added healthcheck for postgres in entrypoint.sh so the web service will wait
    * docker-compose Version 3 doesnt support healthchecks on its own anymore

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /pong/tmp/pids/server.pid
 
+echo "Waiting for db ..."
+declare retry=0
+while !(pg_isready -U postgres -h 127.0.0.1) && $retry < 5; do sleep 1; $retry ++; done;
+
 echo "Preparing Tables ..."
 # depends on custom db:exists Task to check if database exists (lib/tasks/db_exists.rake)
 # Source: https://stackoverflow.com/a/35732641
