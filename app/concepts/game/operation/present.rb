d8f9c978308999b3079096815a2744a23d3b73c9
Tobias Bohn <info@tobiasbohn.com>
2020-02-02 15:24:28 +0100

    [FEATURE] added tests for game present operation

--- app/concepts/game/operation/present.rb
+++ app/concepts/game/operation/present.rb
@@ -1,3 +1,10 @@
+# Public: Operation to present a Game.
+#
+#   Consumer::Operation::Present.(
+#     params: {}
+#   )
+#
+# Returns a Trailblazer::Operation::Result object.
 class Game::Operation::Present < Trailblazer::Operation
   step Model(Game, :new)
   step Contract::Build(constant: Game::Contract::Create)
