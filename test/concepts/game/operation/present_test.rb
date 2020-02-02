f07a6900083c2e2e6099f4ae4b507cbc469ccd27
Tobias Bohn <info@tobiasbohn.com>
2020-02-02 15:24:28 +0100

    [FEATURE] added tests for game present operation

--- test/concepts/game/operation/present_test.rb
+++ test/concepts/game/operation/present_test.rb
@@ -4,7 +4,17 @@ class Game::Operation::PresentTest < ActiveSupport::TestCase
   setup do
   end
 
-  test 'should' do
-
+  test 'should present game' do
+    assert_no_difference 'Game.count' do
+      params = {}
+      assert result = Game::Operation::Present.(params: params)
+      assert result.success?
+      assert model = result[:model]
+      assert_nil model[:id]
+      assert_nil model[:key]
+      assert_nil model[:mode]
+      assert_equal false, model[:protect]
+      assert_nil model[:password_digest]
+    end
   end
 end
